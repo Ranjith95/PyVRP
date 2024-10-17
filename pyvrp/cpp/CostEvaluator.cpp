1266e14aee3245ebbcca7a47ef2490b213327355
Ranjith95 <ranjith19395@yahoo.co.in>
2024-10-16 20:14:25 -0400

    Update CostEvaluator.cpp
    Adding comments

--- pyvrp/cpp/CostEvaluator.cpp
+++ pyvrp/cpp/CostEvaluator.cpp
@@ -2,6 +2,7 @@
 
 using pyvrp::CostEvaluator;
 
+// Cost evaluator function
 CostEvaluator::CostEvaluator(Cost loadPenalty, Cost twPenalty, Cost distPenalty)
     : loadPenalty_(loadPenalty),
       twPenalty_(twPenalty),
